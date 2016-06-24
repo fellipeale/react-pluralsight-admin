3e6f8be77a6f8022e77203165ddb944bff8d891c
falexandre <fellipeale.scopus@outlook.com>
2016-06-24 16:47:11 -0300

    Async writes in redux

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,5 @@
 import * as types from './actionTypes';
-import courseApi from '../api/mockCourseApi';
+import CourseApi from '../api/mockCourseApi';
 
 export function loadCoursesSuccess(courses) {
   return {
@@ -8,11 +8,39 @@ export function loadCoursesSuccess(courses) {
   };
 }
 
+export function createCourseSucess(course) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    course
+  };
+}
+
+export function updateCourseSuccess(course) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    course
+  };
+}
+
 export function loadCourses() {
-  return function(dispatch) {
-    return courseApi.getAllCourses()
+  return dispatch => {
+    return CourseApi.getAllCourses()
       .then(courses => {
         dispatch(loadCoursesSuccess(courses));
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return dispatch => {
+    return CourseApi.saveCourse(course)
+      .then(savedCourse => {
+        course.id ?
+          dispatch(updateCourseSuccess(savedCourse)) :
+          dispatch(createCourseSucess(savedCourse));
       })
       .catch(error => {
         throw(error);
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { browserHistory } from 'react-router';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
 
@@ -8,10 +9,16 @@ class CoursesPage extends Component {
 
   constructor(props, context) {
     super(props, context);
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
+  }
+
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
   }
 
   render() {
@@ -19,6 +26,11 @@ class CoursesPage extends Component {
     return (
       <div>
         <h1>Courses</h1>
+        <input
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage} />
         <CourseList courses={courses} />
       </div>
     );
